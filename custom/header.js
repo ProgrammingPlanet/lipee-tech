a62d377129478f08afa5ce63b3db3567c45a0da8
Vishnu <codevish1811@gmail.com>
2019-04-13 12:05:17 +0530

    final indexand product page

--- custom/header.js
+++
@@ -1,7 +0,0 @@
-
-
-$('.navbar .dropdown').hover(function() {
-	$(this).find('.dropdown-menu').first().stop(true, true).delay(150).slideDown();
-}, function() {
-	$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
-});
